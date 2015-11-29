d3a8a8ec7940931f615faf3f23a36308d6dd663b
andreas_westberg <andreaswestberg91@gmail.com>
2015-11-29 01:25:16 +0100

    Work in progress, read available resources from dir.

--- src/mad-mock/main.go
+++ src/mad-mock/main.go
@@ -5,24 +5,47 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 func main() {
 	var port = flag.String("p", "8080", "What port the mock should run on.")
 	var url = flag.String("u", "", "Base url to system to be mocked (request will be fetched once and stored locally).")
-	var dir = flag.String("d", "./mad-mock", "Directory path to mock data and config files.")
+	var dir = flag.String("d", "mad-mock-store", "Directory path to mock data and config files.")
 	flag.Parse() // parse the flags
 
-	fmt.Println("Port:" + *port + " Url:" + *url + " Dir:" + *dir)
-
+	err := CreateDir(*dir)
+	if err != nil {
+		log.Fatal(err)
+	}
 	mux := http.NewServeMux()
-	mux.HandleFunc("/mock", mockPagehandler)
+	mockPagehandler := MockPagehandler{DataDir: *dir, TargetURL: *url}
+	mux.Handle("/mock", &mockPagehandler)
 	mux.HandleFunc("/", mockhandler)
 	log.Println("Server to listen on a port: ", *port)
 	log.Fatal(http.ListenAndServe(":"+*port, mux))
 }
 
-func mockPagehandler(w http.ResponseWriter, r *http.Request) {
+// CreateDir creates directory relativly to execution path.
+func CreateDir(dir string) error {
+	path, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		return err
+	}
+	dirpath := path + "/" + dir
+	fmt.Println(dirpath)
+	err = os.MkdirAll(dirpath, 0777)
+	return err
+}
+
+// MockPagehandler handles index page.
+type MockPagehandler struct {
+	DataDir   string
+	TargetURL string
+}
+
+func (h *MockPagehandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	title := "Available rescources:"
 	resources := "<ul>"
 	resources = resources + fmt.Sprintf("<div> <p> MOCK <p></div> <br>")
